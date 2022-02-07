f4c9a7d138af148de842ef245958b9a4394fa68a
Raphael Bts <raphael.brethes@outlook.com>
2022-02-07 12:52:57 +0100

    creation of adressepari.js and montlimart

--- server/sandbox.js
+++ server/sandbox.js
@@ -1,7 +1,10 @@
 /* eslint-disable no-console, no-process-exit */
 const dedicatedbrand = require('./sources/dedicatedbrand');
+const montelimard = require('./sources/montlimartbrand');
+const dedicatedbrand = require('./sources/adresseparisbrand');
 
-async function sandbox (eshop = 'https://www.dedicatedbrand.com/en/men/news') {
+
+async function sandbox (eshop = ['https://www.dedicatedbrand.com/en/men/news','https://www.montlimart.com/toute-la-collection.html','') {
   try {
     console.log(`🕵️‍♀️  browsing ${eshop} source`);
 
