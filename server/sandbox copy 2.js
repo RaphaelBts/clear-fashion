f6721b57ef442b3c73fab2a970ffabe75fd2d1bc
Raphael Bts <raphael.brethes@outlook.com>
2022-02-07 12:53:24 +0100

    adresseparisbrand montlimartbrand

---
+++ server/sandbox copy 2.js
@@ -0,0 +1,22 @@
+/* eslint-disable no-console, no-process-exit */
+const dedicatedbrand = require('./sources/adresseparisbrand');
+
+
+async function sandbox (eshop = 'https://adresse.paris/630-toute-la-collection') {
+  try {
+    console.log(`🕵️‍♀️  browsing ${eshop} source`);
+
+    const products = await dedicatedbrand.scrape(eshop);
+
+    console.log(products);
+    console.log('done');
+    process.exit(0);
+  } catch (e) {
+    console.error(e);
+    process.exit(1);
+  }
+}
+
+const [,, eshop] = process.argv;
+
+sandbox(eshop);
