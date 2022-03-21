4e44c34b6dbdea0e6f38709b53a057ae2deee293
Raphael Bts <raphael.brethes@outlook.com>
2022-03-21 17:21:14 +0100

    re switch to api.js working with position products/search to the top , add page size

--- server/api.js
+++ server/api.js
@@ -37,6 +37,27 @@ app.get('/', (request, response) => {
   response.send({'ack': true});
 });
 // get one product
+app.get('/products/search', (req,res) => {
+  const size = parseInt(req.query.limit) || 12;
+  const brand = req.query.brand || '';
+  const price = parseInt(req.query.price) || -1;
+  const page = parseInt(req.query.page)
+  const currentPage = page 
+  const pageSize = size 
+  const totalCount =  collection.countDocuments()
+  const countPages = totalCount /size 
+  // currentpage = page
+  // count (all items)
+  //count (items par page)
+  //CountPages 
+
+  collection.find({ 'brand': brand }, { 'price': { $lte: price } }).skip(page > 0 ? ( ( page - 1 ) * size) : 0).limit(size).toArray()
+      .then(results => res.status(200).json({"results":res,"meta":paginate(page,totalCount,size,)}))
+      .catch(err => {
+          console.log(err)
+          throw err
+      })
+});
 app.get('/products/:id', async (req,res) => {  // Error: Failure when receiving data from the peer
   try {
       const results = await collection.find({'_id': new ObjectId(req.params.id)}).toArray()
@@ -45,7 +66,7 @@ app.get('/products/:id', async (req,res) => {  // Error: Failure when receiving 
       console.log(err)
       throw err
   }
-})
+});
 // get all the products 
 app.get('/products', (req,res) => {
   collection.find({}).toArray()
@@ -54,7 +75,7 @@ app.get('/products', (req,res) => {
           console.log(err)
           throw err
       })
-})
+});
 app.get('/products/brand/:brand', (req,res) => { // search products from specific brand http://localhost:8092/products/brand/dedicated
 
   collection.find({ 'brand': req.params.brand } ).toArray()
@@ -77,20 +98,23 @@ app.get('/products/brand/:brand', (req,res) => { // search products from specifi
 //       'price': price
 //   });
 // });
-app.get('/products/search', (req,res) => { // search products from specific brand and price <=priced specified
+// app.get('/products/search', (req,res) => { // search products from specific brand and price <=priced specified
 
-  const brand = req.query.brand ; 
-  //const price = req.query.price;
-  // let limit = req.query.limit;
+//   const brand = req.query.brand ; 
+//   console.log(brand);
+//   //const price = req.query.price;
+//   // let limit = req.query.limit;
 
-  collection.find({ 'brand': brand }).toArray()
-      .then(results =>{res.send(results)
-        console.log(results)})
-      .catch(err => {
-          console.log(err)
-          throw err
-      })
-})
+//   collection.find({'brand': brand }).toArray()
+//       .then(results =>{res.send(results)
+//         console.log(results)})
+//       .catch(err => {
+//           console.log(err)
+//           console.log(brand)
+//           throw err
+//       })
+// })
+
 
 //const products = await collection.find({price : {$lt:price}}).toArray();
 app.get('/exit', function (req,res) {
